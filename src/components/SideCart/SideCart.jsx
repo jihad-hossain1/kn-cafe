8249e5cbaabf7a99ba04e7a96e4ca25d630aa949
jihad-hossain1 <jihadkhan934@gmail.com>
2023-04-02 03:48:58 +0600

    local storage added

--- src/components/SideCart/SideCart.jsx
+++ src/components/SideCart/SideCart.jsx
@@ -4,10 +4,16 @@ const SideCart = (props) => {
     const cart = props.cart;
     // console.log(props);
     const times = props.times;
-    console.log(cart);
+    // console.log(cart);
     let total = 0;
+    let quantity = 0;
     for (const tim of times) {
         total = total + tim.readTime
+
+    }
+    for (const qty of cart) {
+        // qty.quantity = qty.quantity || 1;
+        quantity = quantity + qty.quantity 
     }
 
     // let totalTime = 0;
@@ -19,9 +25,9 @@ const SideCart = (props) => {
         <>
             <div className="sticky top-0 ">
                 <div className="bg-slate-200 rounded p-2">
-                    <h2>Time spend: {total} min</h2>
-                    <h3>Bookmarked: {cart.length} </h3>
-                    <p>Qty: </p>
+                    <h2>Total spend time: {total} min</h2>
+                    <h3>Bookmarked items: {cart.length} </h3>
+                    <p>TotalBK-Qty: { quantity}</p>
                 </div>
                 <div className="bg-slate-200  py-1">
                     {
